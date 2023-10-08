79dd0f86e37fb5c214bea0d6e6cb6264847ed82c
William Chang <wilicw1346@gmail.com>
2023-10-09 00:01:01 +0800

    feat: create a task for LoRa

--- main/main.c
+++ main/main.c
@@ -1,11 +1,17 @@
+#include <bsp.h>
 #include <driver/gpio.h>
 #include <driver/spi_master.h>
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
 #include <ra01s.h>
+#include <stddef.h>
 #include <stdint.h>
 
-void spi_init(spi_host_device_t device, uint32_t mosi, uint32_t miso, uint32_t sck) {
+static const char *TAG = "main";
+static const size_t RECV_LEN = 255;
+
+void spi_init(spi_host_device_t device, uint32_t mosi, uint32_t miso,
+              uint32_t sck) {
   esp_err_t ret;
   const spi_bus_config_t spi_bus_config = {
       .sclk_io_num = sck,
@@ -18,13 +24,30 @@ void spi_init(spi_host_device_t device, uint32_t mosi, uint32_t miso, uint32_t s
   ESP_LOGI(TAG, "SPI%d_HOST spi_bus_initialize=%d", device + 1, ret);
 }
 
-void app_main(void) {
-  spi_init(LORA_SPI_HOST, CONFIG_LORA_MOSI_GPIO, CONFIG_LORA_MISO_GPIO, CONFIG_LORA_SCK_GPIO);
-  lora_init();
-
+void task(void *pvParameters) {
   uint8_t message[] = "Hello World";
+  size_t len;
+  uint8_t data[RECV_LEN];
+  uint32_t last_send = xTaskGetTickCount();
   while (1) {
-    LoRaSend(message, sizeof(message), SX126x_TXMODE_SYNC);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    if ((len = LoRaReceive(&data[0], RECV_LEN)) > 0) {
+      for (size_t i = 0; i < len; i++)
+        printf("%c", data[i]);
+      puts("");
+    }
+    if (xTaskGetTickCount() - last_send >= pdMS_TO_TICKS(1000)) {
+      printf("i = %lu\n", xTaskGetTickCount());
+      LoRaSend(message, sizeof(message), SX126x_TXMODE_SYNC);
+      last_send = xTaskGetTickCount();
+    }
+    vTaskDelay(pdMS_TO_TICKS(1));
   }
+}
+
+void app_main(void) {
+  vTaskDelay(5000 / portTICK_PERIOD_MS);
+  spi_init(LORA_SPI_HOST, CONFIG_LORA_MOSI_GPIO, CONFIG_LORA_MISO_GPIO,
+           CONFIG_LORA_SCK_GPIO);
+  lora_init();
+  xTaskCreate(task, "task", 4096, NULL, 1, NULL);
 }
